20a4813e5471bfaa532baddd7c02b8a287a4f4a1
Gupta <agupt196@LAMU02YQ2SXLVCJ.uhc.com>
2019-10-27 15:50:14 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,61 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class DatatypeInput extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      numerator: "",
+      denominator: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event){
+    alert('Form submitted! Input value: \nNumerator: ' + this.state.numerator + "\nDenominator: " + this.state.denominator);
+    event.preventDefault();
+  }
+
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit}>
+        
+        <div name="numerator-div" className="input-divs">
+          <label for="num" className="input-lbl"> Numerator:</label>
+          <select className="datatype-inputs" name="numerator" id="num" value={this.state.numerator} onChange={this.handleChange}>
+            <option value="decimal">Decimal</option>
+            <option value="int">Int</option>
+            <option value="float">Float</option>
+            <option value="double">Double</option>
+          </select>
+        </div>
+        
+        <div name="denominator-div" className="input-divs">
+          <label for="denom" className="input-lbl">Denominator:</label>
+          <input className="datatype-inputs" name="denominator" id="denom" value={this.state.denominator} onChange={this.handleChange}/>
+        </div>
+
+        <input type="submit" value="Submit" className="submit-btn" />
+      </form>
+    );
+  }
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DatatypeInput />
     </div>
   );
 }
