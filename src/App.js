83def51eaadd66f98a3e51715adeac165ff65564
AdarshGupta <adarsh035@gmail.com>
2019-11-17 02:07:41 +0530

    Support div computation; extract decimal interpret function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function interpretDecimalInput(inputType, inputPrecision, inputScale){
+  let precision = 0;
+  let scale = 0;
+
+  if(inputType === "decimal"){
+    if(inputPrecision){
+      precision = parseInt(inputPrecision);
+    }
+    else{
+      alert("Please specify the 'Precision' value for the decimal data type."); //else the default value of 18 is used
+      return;
+      // fop_precision = 18;
+    }
+
+    if(inputScale){
+      scale = parseInt(inputScale);
+    }
+    else{
+      alert("Please specify the 'Scale' value for the decimal data type."); // else the default value of 0 is used
+      return;
+      // fop_scale = 0;
+    }
+  }
+  else{
+    if(inputType === "smallint"){
+      precision = 5;
+    }
+    else if(inputType === "int"){
+      precision = 10;
+    }
+    else if(inputType === "bigint"){
+      precision = 19;
+    }
+    else{
+      alert("Invalid data type: " + inputType);
+    }
+    scale = 0;
+  }
+
+  return {"precision": precision, "scale": scale};
+}
+
+
 function calcResultantDecimal(calcDetails){
   const fop = calcDetails.first_operand;
   const sop = calcDetails.second_operand;
@@ -13,78 +56,87 @@ function calcResultantDecimal(calcDetails){
   let result_precision = 0;
   let result_scale = 0;
 
-  if(fop === "decimal"){
-    if(calcDetails.fop_precision_input){
-      fop_precision = parseInt(calcDetails.fop_precision_input);
-    }
-    else{
-      alert("Please specify the 'Precision' value for the decimal data type of first operand else the default value of 18 is used.");
-      return;
-      // fop_precision = 18;
-    }
+  let fop_decimal = interpretDecimalInput(fop, calcDetails.fop_precision_input, calcDetails.fop_scale_input);
+  fop_precision = fop_decimal.precision;
+  fop_scale = fop_decimal.scale;
+  
 
-    if(calcDetails.fop_scale_input){
-      fop_scale = parseInt(calcDetails.fop_scale_input);
-    }
-    else{
-      alert("Please specify the 'Scale' value for the decimal data type of first operand else the default value of 0 is used.");
-      return;
-      // fop_scale = 0;
-    }
-  }
-  else{
-    if(fop === "smallint"){
-      fop_precision = 5;
-    }
-    else if(fop === "int"){
-      fop_precision = 10;
-    }
-    else if(fop === "bigint"){
-      fop_precision = 19;
-    }
-    else{
-      alert("Invalid data type: " + fop);
-    }
-    fop_scale = 0;
-  }
+  // if(fop === "decimal"){
+  //   if(calcDetails.fop_precision_input){
+  //     fop_precision = parseInt(calcDetails.fop_precision_input);
+  //   }
+  //   else{
+  //     alert("Please specify the 'Precision' value for the decimal data type of first operand."); //else the default value of 18 is used
+  //     return;
+  //     // fop_precision = 18;
+  //   }
 
-  if(sop === "decimal"){
-    if(calcDetails.sop_precision_input){
-      sop_precision = parseInt(calcDetails.sop_precision_input);
-    }
-    else{
-      alert("Please specify the 'Precision' value for the decimal data type of second operand else the default value of 18 is used.");
-      return;
-      // sop_precision = 18;
-    }
+  //   if(calcDetails.fop_scale_input){
+  //     fop_scale = parseInt(calcDetails.fop_scale_input);
+  //   }
+  //   else{
+  //     alert("Please specify the 'Scale' value for the decimal data type of first operand."); // else the default value of 0 is used
+  //     return;
+  //     // fop_scale = 0;
+  //   }
+  // }
+  // else{
+  //   if(fop === "smallint"){
+  //     fop_precision = 5;
+  //   }
+  //   else if(fop === "int"){
+  //     fop_precision = 10;
+  //   }
+  //   else if(fop === "bigint"){
+  //     fop_precision = 19;
+  //   }
+  //   else{
+  //     alert("Invalid data type: " + fop);
+  //   }
+  //   fop_scale = 0;
+  // }
 
-    if(calcDetails.sop_scale_input){
-      sop_scale = parseInt(calcDetails.sop_scale_input);
-    }
-    else{
-      alert("Please specify the 'Scale' value for the decimal data type of second operand else default value of 0 is used.");
-      return;
-      // sop_scale = 0;
-    }
-  }
-  else{
-    if(sop === "smallint"){
-      sop_precision = 5;
-    }
-    else if(sop === "int"){
-      sop_precision = 10;
-    }
-    else if(sop === "bigint"){
-      sop_precision = 19;
-    }
-    else{
-      alert("Invalid data type: " + sop);
-    }
-    sop_scale = 0;
-  }
+  let sop_decimal = interpretDecimalInput(sop, calcDetails.sop_precision_input, calcDetails.sop_scale_input);
+  sop_precision = sop_decimal.precision;
+  sop_scale = sop_decimal.scale;
+
+  // if(sop === "decimal"){
+  //   if(calcDetails.sop_precision_input){
+  //     sop_precision = parseInt(calcDetails.sop_precision_input);
+  //   }
+  //   else{
+  //     alert("Please specify the 'Precision' value for the decimal data type of second operand."); // else the default value of 18 is used
+  //     return;
+  //     // sop_precision = 18;
+  //   }
+
+  //   if(calcDetails.sop_scale_input){
+  //     sop_scale = parseInt(calcDetails.sop_scale_input);
+  //   }
+  //   else{
+  //     alert("Please specify the 'Scale' value for the decimal data type of second operand"); // else default value of 0 is used.
+  //     return;
+  //     // sop_scale = 0;
+  //   }
+  // }
+  // else{
+  //   if(sop === "smallint"){
+  //     sop_precision = 5;
+  //   }
+  //   else if(sop === "int"){
+  //     sop_precision = 10;
+  //   }
+  //   else if(sop === "bigint"){
+  //     sop_precision = 19;
+  //   }
+  //   else{
+  //     alert("Invalid data type: " + sop);
+  //   }
+  //   sop_scale = 0;
+  // }
 
   if(fop_scale > fop_precision || sop_scale > sop_precision){
-    alert("The scale value must be less than or equal to the precision value.")
+    alert("The scale value must be less than or equal to the precision value.");
     return;
   }
 
@@ -99,11 +151,24 @@ function calcResultantDecimal(calcDetails){
   else if(operator === "divi"){
     result_scale = Math.max(4, fop_scale + sop_precision - sop_scale + 1);
     result_precision = fop_precision - fop_scale + sop_scale + result_scale;
+
+    if(result_scale > 100){
+      result_precision = result_precision - (result_scale - 37);
+      result_scale = 37;
+    }
+
+    if(result_precision > 38){
+      result_precision = 38;
+      result_scale = Math.max(38 + result_scale - result_precision, 4);
+    }
   }
   else{
     alert("Operator not supported yet!");
     return;
   }
+
+  result_precision = Math.min(38, result_precision)
+  result_scale = Math.min(37, result_scale)
 
   return "decimal(" + result_precision + ", " + result_scale + ")";
 }
@@ -128,7 +193,7 @@ class DatatypeInput extends React.Component{
 
   handleChange(event, data){
     const target = event.target;
-    const name = target.getAttribute('name');;
+    const name = target.getAttribute('name');
     let value = data === undefined ? target.value : data;
     console.log("value: " + value);
     if((name === "fop_precision_input" || name === "sop_precision_input") && parseInt(value) > 38){
@@ -186,8 +251,7 @@ class DatatypeInput extends React.Component{
       this.setState({
         resultant_datatype: result_dt
       });
-    } 
-    event.preventDefault();
+    }
   }
 
   render(){
@@ -226,9 +290,11 @@ class DatatypeInput extends React.Component{
               <label for="sop" className="input-lbl">Second Operand:</label>
               <select className="datatype-inputs" name="second_operand" id="sop" value={this.state.second_operand} onChange={this.handleChange}>
                 <option value="decimal">Decimal</option>
-                <option value="int">Int</option>
-                <option value="float">Float</option>
-                <option value="double">Double</option>
+                <option value="smallint">SmallInt/Int2</option>
+                <option value="int">Int/Int4</option>
+                <option value="bigint">BigInt/Int8</option>
+                <option value="float">Float/Float4</option>
+                <option value="double">Double/Float8</option>
               </select>
 
               <label for="precision_input" className={this.state.second_operand === "decimal" ? "prec_arg_lbl" : "prec_arg_lbl element_hidden" }> Precision</label>
@@ -243,6 +309,8 @@ class DatatypeInput extends React.Component{
               <input type="submit" value="Submit" className="submit-btn" />
               <div className="resultant-datatype">{this.state.resultant_datatype.toUpperCase()}</div>  
             </div>
+
+            <div className="footer">Made with &hearts; by Adarsh</div>
           </form>
         </div>
       </div>
