ef6db903df3f2c0e49f228de54874d398c00ec55
AdarshGupta <adarsh035@gmail.com>
2019-11-24 23:42:02 +0530

    Refactoring and abstracting datatype input component

--- src/App.js
+++ src/App.js
@@ -2,51 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function interpretDecimalInput(inputType, inputPrecision, inputScale){
+function interpretDecimalInput(inputType, inputPrecision, inputScale) {
   let precision = 0;
   let scale = 0;
 
-  if(inputType === "decimal"){
-    if(inputPrecision){
+  if (inputType === "decimal") {
+    if (inputPrecision) {
       precision = parseInt(inputPrecision);
     }
-    else{
+    else {
       alert("Please specify the 'Precision' value for the decimal data type."); //else the default value of 18 is used
       return;
       // fop_precision = 18;
     }
 
-    if(inputScale){
+    if (inputScale) {
       scale = parseInt(inputScale);
     }
-    else{
+    else {
       alert("Please specify the 'Scale' value for the decimal data type."); // else the default value of 0 is used
       return;
       // fop_scale = 0;
     }
   }
-  else{
-    if(inputType === "smallint"){
+  else {
+    if (inputType === "smallint") {
       precision = 5;
     }
-    else if(inputType === "int"){
+    else if (inputType === "int") {
       precision = 10;
     }
-    else if(inputType === "bigint"){
+    else if (inputType === "bigint") {
       precision = 19;
     }
-    else{
+    else {
       alert("Invalid data type: " + inputType);
       return;
     }
     scale = 0;
   }
 
-  return {"precision": precision, "scale": scale};
+  return { "precision": precision, "scale": scale };
 }
 
 
-function calcResultantDecimal(calcDetails){
+function calcResultantDecimal(calcDetails) {
   const fop = calcDetails.first_operand;
   const sop = calcDetails.second_operand;
   const operator = calcDetails.operator;
@@ -57,52 +57,52 @@ function calcResultantDecimal(calcDetails){
   let result_precision = 0;
   let result_scale = 0;
 
-  let fop_decimal = interpretDecimalInput(fop, calcDetails.fop_precision_input, calcDetails.fop_scale_input);
-  if(fop_decimal){
+  let fop_decimal = interpretDecimalInput(fop, calcDetails.first_precision_input, calcDetails.first_scale_input);
+  if (fop_decimal) {
     fop_precision = fop_decimal.precision;
     fop_scale = fop_decimal.scale;
   }
-  else{
+  else {
     return;
   }
 
-  let sop_decimal = interpretDecimalInput(sop, calcDetails.sop_precision_input, calcDetails.sop_scale_input);
-  if(sop_decimal){
+  let sop_decimal = interpretDecimalInput(sop, calcDetails.second_precision_input, calcDetails.second_scale_input);
+  if (sop_decimal) {
     sop_precision = sop_decimal.precision;
     sop_scale = sop_decimal.scale;
   }
-  else{
+  else {
     return;
   }
 
-  if(fop_scale > fop_precision || sop_scale > sop_precision){
+  if (fop_scale > fop_precision || sop_scale > sop_precision) {
     alert("The scale value must be less than or equal to the precision value.");
     return;
   }
 
-  if(operator === "add" || operator === "sub"){
+  if (operator === "add" || operator === "sub") {
     result_scale = Math.max(fop_scale, sop_scale)
     result_precision = Math.max(fop_precision - fop_scale, sop_precision - sop_scale) + 1 + result_scale;
   }
-  else if(operator === "mul"){
+  else if (operator === "mul") {
     result_scale = fop_scale + sop_scale;
     result_precision = fop_precision + sop_precision + 1;
   }
-  else if(operator === "divi"){
+  else if (operator === "divi") {
     result_scale = Math.max(4, fop_scale + sop_precision - sop_scale + 1);
     result_precision = fop_precision - fop_scale + sop_scale + result_scale;
 
-    if(result_scale > 100){
+    if (result_scale > 100) {
       result_precision = result_precision - (result_scale - 37);
       result_scale = 37;
     }
 
-    if(result_precision > 38){
+    if (result_precision > 38) {
       result_precision = 38;
       result_scale = Math.max(38 + result_scale - result_precision, 4);
     }
   }
-  else{
+  else {
     alert("Operator not supported yet!");
     return;
   }
@@ -113,35 +113,59 @@ function calcResultantDecimal(calcDetails){
   return "decimal(" + result_precision + ", " + result_scale + ")";
 }
 
-class DatatypeInput extends React.Component{
-  constructor(props){
+function DatatypeInput(props) {
+  return (
+    <div id={props.operand + "-operand-div"} className="input-divs">
+      <label for={props.operand} className="input-lbl" id={props.operand + "-lbl"} > {props.operand} Operand:</label>
+      <select className="datatype-inputs" name={props.operand + "_operand"} id={props.operand} value={props.operandValue} onChange={props.handleChange}>
+        <option value="decimal">Decimal</option>
+        <option value="smallint">SmallInt/Int2</option>
+        <option value="int">Int/Int4</option>
+        <option value="bigint">BigInt/Int8</option>
+        <option value="float">Float/Float4</option>
+        <option value="double">Double/Float8</option>
+      </select>
+
+      <label for="precision_input" className={"prec_arg_lbl" + (props.operandValue === "decimal" ? "" : " element_hidden")}> Precision</label>
+      <input type="text" name={props.operand + "_precision_input"} id="precision_input" className={props.operandValue === "decimal" ? "" : "element_hidden"} value={props.precision_input} onChange={props.handleChange} />
+
+      <label for="scale_input" className={"scale_arg_lbl" + (props.operandValue === "decimal" ? "" : " element_hidden")}> Scale</label>
+      <input type="text" name={props.operand + "_scale_input"} className={"scale_input" + (props.operandValue === "decimal" ? "" : " element_hidden")} value={props.scale_input} onChange={props.handleChange} />
+    </div>
+  );
+}
+
+
+class CalculatorForm extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       first_operand: "decimal",
       second_operand: "decimal",
-      fop_precision_input: "",
-      fop_scale_input: "",
-      sop_precision_input: "",
-      sop_scale_input: "",
+      first_precision_input: "",
+      first_scale_input: "",
+      second_precision_input: "",
+      second_scale_input: "",
       operator: "",
       resultant_datatype: "Resultant Data type"
-    }
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event, data){
+  handleChange(event, data) {
     const target = event.target;
     const name = target.getAttribute('name');
     let value = data === undefined ? target.value : data;
+    console.log("name: " + name);
     console.log("value: " + value);
-    if((name === "fop_precision_input" || name === "sop_precision_input") && parseInt(value) > 38){
+    if ((name === "first_precision_input" || name === "second_precision_input") && parseInt(value) > 38) {
       alert("Max. precision possible is 38");
       value = 38;
     }
 
-    if((name === "fop_scale_input" || name === "sop_scale_input") && parseInt(value) > 37){
+    if ((name === "first_scale_input" || name === "second_scale_input") && parseInt(value) > 37) {
       alert("Max. scale possible is 37.");
       value = 37;
     }
@@ -151,103 +175,70 @@ class DatatypeInput extends React.Component{
     });
   }
 
-  handleSubmit(event){
-    // alert('Form submitted! Input value: \nFirst Operand: ' + this.state.first_operand + "\nSecond Operand: " + this.state.second_operand);
+  handleSubmit(event) {
+    event.preventDefault();
+
     const fop = this.state.first_operand;
     const sop = this.state.second_operand;
     let result_dt = "";
-    event.preventDefault();
 
-    if(!this.state.operator){
+    if (!this.state.operator) {
       alert("Please select the operator involved in the computation by clicking on it.");
       return;
     }
-    
-    if(fop === "double" || sop === "double"){
+
+    if (fop === "double" || sop === "double") {
       result_dt = "double";
     }
-    else if(fop === "float" || sop === "float"){
-      if(fop === sop){
+    else if (fop === "float" || sop === "float") {
+      if (fop === sop) {
         result_dt = "float";
       }
-      else{
+      else {
         result_dt = "double";
       }
     }
-    else if (fop === "decimal" || sop === "decimal"){
+    else if (fop === "decimal" || sop === "decimal") {
       result_dt = calcResultantDecimal(this.state);
     }
-    else if (fop === "bigint" || sop === "bigint"){
+    else if (fop === "bigint" || sop === "bigint") {
       result_dt = "bigint";
     }
-    else if (fop === "int" || sop === "int"){
+    else if (fop === "int" || sop === "int") {
       result_dt = "int";
     }
-    else{
+    else {
       result_dt = "smallint";
     }
 
-    if(result_dt){
+    if (result_dt) {
       this.setState({
         resultant_datatype: result_dt
       });
     }
   }
 
-  render(){
+  render() {
     return (
       <div>
         <div className="App-header">Redshift Calculator</div>
         <div>
           <form onSubmit={this.handleSubmit}>
-            
-            <div id="first-operand-div" className="input-divs">
-              <label for="fop" className="input-lbl" id ="fop-lbl" > First Operand:</label>
-              <select className="datatype-inputs" name="first_operand" id="fop" value={this.state.first_operand} onChange={this.handleChange}>
-                <option value="decimal">Decimal</option>
-                <option value="smallint">SmallInt/Int2</option>
-                <option value="int">Int/Int4</option>
-                <option value="bigint">BigInt/Int8</option>
-                <option value="float">Float/Float4</option>
-                <option value="double">Double/Float8</option>
-              </select>
 
-              <label for="precision_input" className={this.state.first_operand === "decimal" ? "prec_arg_lbl" : "prec_arg_lbl element_hidden" }> Precision</label>
-              <input type="text" name="fop_precision_input" id="precision_input" className={this.state.first_operand === "decimal" ? "" : "element_hidden" } value={this.state.fop_precision_input} onChange={this.handleChange} />
-
-              <label for="scale_input" className={this.state.first_operand === "decimal" ? "scale_arg_lbl" : "scale_arg_lbl element_hidden" }> Scale</label>
-              <input type="text" name="fop_scale_input" className={this.state.first_operand === "decimal" ? "scale_input" : "scale_input element_hidden" } value={this.state.fop_scale_input} onChange={this.handleChange} />
-            </div>
+            <DatatypeInput operand="first" operandValue={this.state.first_operand} precision_input={this.state.first_precision_input} scale_input={this.state.first_scale_input} handleChange={this.handleChange} />
 
             <ul className="operator-container">
-              <li name="operator" className={this.state.operator === "add" ? "operator-item selected" : "operator-item"} onClick={e => this.handleChange(e, "add")}>+</li>
-              <li name="operator" className={this.state.operator === "sub" ? "operator-item selected" : "operator-item"} onClick={e => this.handleChange(e, "sub")}>-</li>
-              <li name="operator" className={this.state.operator === "divi" ? "operator-item selected" : "operator-item"} onClick={e => this.handleChange(e, "divi")}>/</li>
-              <li name="operator" className={this.state.operator === "mul" ? "operator-item selected" : "operator-item"} onClick={e => this.handleChange(e, "mul")}>X</li>
+              <li name="operator" className={"operator-item" + (this.state.operator === "add" ? " selected" : "")} onClick={e => this.handleChange(e, "add")}>+</li>
+              <li name="operator" className={"operator-item" + (this.state.operator === "sub" ? " selected" : "")} onClick={e => this.handleChange(e, "sub")}>-</li>
+              <li name="operator" className={"operator-item" + (this.state.operator === "divi" ? " selected" : "")} onClick={e => this.handleChange(e, "divi")}>/</li>
+              <li name="operator" className={"operator-item" + (this.state.operator === "mul" ? " selected" : "")} onClick={e => this.handleChange(e, "mul")}>X</li>
             </ul>
-            
-            <div id="second-operand-div" className="input-divs">
-              <label for="sop" className="input-lbl">Second Operand:</label>
-              <select className="datatype-inputs" name="second_operand" id="sop" value={this.state.second_operand} onChange={this.handleChange}>
-                <option value="decimal">Decimal</option>
-                <option value="smallint">SmallInt/Int2</option>
-                <option value="int">Int/Int4</option>
-                <option value="bigint">BigInt/Int8</option>
-                <option value="float">Float/Float4</option>
-                <option value="double">Double/Float8</option>
-              </select>
 
-              <label for="precision_input" className={this.state.second_operand === "decimal" ? "prec_arg_lbl" : "prec_arg_lbl element_hidden" }> Precision</label>
-              <input type="text" name="sop_precision_input" id="precision_input" className={this.state.second_operand === "decimal" ? "" : "element_hidden" } value={this.state.sop_precision_input} onChange={this.handleChange} />
+            <DatatypeInput operand="second" operandValue={this.state.second_operand} precision_input={this.state.second_precision_input} scale_input={this.state.second_scale_input} handleChange={this.handleChange} />
 
-              <label for="scale_input" className={this.state.second_operand === "decimal" ? "scale_arg_lbl" : "scale_arg_lbl element_hidden" }> Scale</label>
-              <input type="text" name="sop_scale_input" className={this.state.second_operand === "decimal" ? "scale_input" : "scale_input element_hidden" } value={this.state.sop_scale_input} onChange={this.handleChange} />
-            </div>
-
-            
             <div className="resultant-div">
               <input type="submit" value="Submit" className="submit-btn" />
-              <div className="resultant-datatype">{this.state.resultant_datatype.toUpperCase()}</div>  
+              <div className="resultant-datatype">{this.state.resultant_datatype.toUpperCase()}</div>
             </div>
 
             <div className="footer">Made with &hearts; by Adarsh</div>
@@ -260,7 +251,7 @@ class DatatypeInput extends React.Component{
 function App() {
   return (
     <div className="App">
-      <DatatypeInput />
+      <CalculatorForm />
     </div>
   );
 }
