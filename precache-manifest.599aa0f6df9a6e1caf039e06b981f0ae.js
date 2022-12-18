820c670eeca58cc47f29647fd87bfc1a1f05db26
Adarsh Gupta <adarsh035@gmail.com>
2022-12-18 14:10:36 +0530

    Updates

---
+++ precache-manifest.599aa0f6df9a6e1caf039e06b981f0ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d39a87fb3779f6e64241c891c65c72",
+    "url": "/Redshift-calc/index.html"
+  },
+  {
+    "revision": "29a152ba2ee161ea6949",
+    "url": "/Redshift-calc/static/css/main.e634d082.chunk.css"
+  },
+  {
+    "revision": "af4cea8e3a0e3a7f5de6",
+    "url": "/Redshift-calc/static/js/2.654bc264.chunk.js"
+  },
+  {
+    "revision": "29a152ba2ee161ea6949",
+    "url": "/Redshift-calc/static/js/main.e92f0078.chunk.js"
+  },
+  {
+    "revision": "e1f4ddd599edf6284627",
+    "url": "/Redshift-calc/static/js/runtime-main.bfc478c3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Redshift-calc/static/media/logo.25bf045c.svg"
+  }
+]);
